4ef95272263fe78ffff560b817770dd2c0462d3a
pphatt <vutienphat0809@gmail.com>
2025-01-06 22:03:57 +0700

    feat: move serilog config into appsettings.Development.json

--- Server.API/Extensions/HostBuilderExtension.cs
+++ Server.API/Extensions/HostBuilderExtension.cs
@@ -8,11 +8,7 @@ public static class HostBuilderExtension
     public static ConfigureHostBuilder AddLogging(this ConfigureHostBuilder host)
     {
         host.UseSerilog((context, configuration) =>
-            configuration
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM-yyyy HH:mm:ss} {Level:u3}] [{SourceContext}]{NewLine}{Message:lj}{NewLine}{Exception}")
+            configuration.ReadFrom.Configuration(context.Configuration)
         );
 
         return host;
--- Server.API/obj/Debug/net8.0/Server.API.AssemblyInfo.cs
+++ Server.API/obj/Debug/net8.0/Server.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ece352d382c30500d0be234cfaaaf850b7dae88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
